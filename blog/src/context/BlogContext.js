21da1f76034f588a107608e1f9bde9312f2126c0
anjana6 <anjanashakthi95@gmail.com>
2020-03-17 16:48:40 +0530

    create delete post part

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -5,7 +5,9 @@ const BlogContext = React.createContext();
 const blogReducer = (state,action) =>{
     switch(action.type){
         case'ADD_BLOGPOST':
-            return [...state,{title: `Blog Post #${state.length+1}`}];
+            return [...state,{id: Math.floor(Math.random()*99999),title: `Blog Post #${state.length+1}`}];
+        case 'DELETE_BLOGPOST':
+            return state.filter((blogPost) => blogPost.id !== action.Payload);
         default:
             return state;
     }
@@ -19,10 +21,41 @@ export const BlogProvider = ({children}) => {
             type: 'ADD_BLOGPOST',
         })
     };
+    const deleteBlogPost = (id) =>{
+        dispatch({
+            type:'DELETE_BLOGPOST',
+            Payload: id
+        })
+    };
 
-    return <BlogContext.Provider value={{data:blogPosts, addBlogPost}}>
+    return <BlogContext.Provider value={{data:blogPosts, addBlogPost,deleteBlogPost}}>
         {children}
     </BlogContext.Provider>;
 };
 
 export default BlogContext;
+
+
+
+// import React, { useReducer } from 'react';
+// import createDataContext from './createDataContext';
+
+
+// const blogReducer = (state,action) =>{
+//     switch(action.type){
+//         case'ADD_BLOGPOST':
+//             return [...state,{title: `Blog Post #${state.length+1}`}];
+//         default:
+//             return state;
+//     }
+// }
+
+// const addBlogPost = dispatch => {
+//     return () =>{
+//         dispatch({type: 'ADD_BLOGPOST'});
+//     };
+    
+// };
+
+
+// export const {Context,Provider} = createDataContext(blogReducer,{addBlogPost},[]);
--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,22 +1,46 @@
 import React,{useContext} from 'react'
-import {View,Text,StyleSheet, Button,FlatList} from 'react-native';
+import {View,Text,StyleSheet, Button,FlatList,TouchableOpacity} from 'react-native';
 import BlogContext from '../context/BlogContext';
+import {Feather} from '@expo/vector-icons'; 
 
 const IndexScreen = ({navigation}) => {
-    const {data,addBlogPost} = useContext(BlogContext);
+    const {data,addBlogPost,deleteBlogPost} = useContext(BlogContext);
     return (
         <View>
-            <Text>Home Screen</Text>
             <Button title="Add Post" onPress={addBlogPost} />
-            <FlatList
+            <FlatList 
                 data={data}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({item}) =>{
-                return <Text>{item.title}</Text>
+                return (
+                    <View style={styles.row}>
+                        <Text style={styles.title}>{item.title}-{item.id}</Text>
+                        <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                            <Feather name="trash" style={styles.icon}/>
+                        </TouchableOpacity>
+                    </View> 
+                );
                 }}
             />
         </View>
     )
-}
+};
+
+const styles = StyleSheet.create({
+    row:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingVertical:20,
+        paddingHorizontal:10,
+        borderTopWidth:1,
+        borderColor:'gray'
+    },
+    title:{
+        fontSize:18
+    },
+    icon:{
+        fontSize:24
+    }
+})
 
 export default IndexScreen;
